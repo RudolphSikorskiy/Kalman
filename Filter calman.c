30340c330f2d6ae793e4d416fbc385958990abe1
RudolphSikorskiy <numbern19@gmail.com>
2018-05-23 11:09:08 +0300

    Kalman

---
+++ Filter calman.c
@@ -0,0 +1,26 @@
+// переменные для калмана
+float varVolt = 0.25;  		// среднее отклонение 
+float varProcess = 0.05; // скорость реакции на изменение (подбирается вручную)
+float Pc = 0.0;
+float G = 0.0;
+float P = 1.0;
+float Xp = 0.0;
+float Zp = 0.0;
+float Xe = 0.0;
+// переменные для калмана
+
+int main(void)
+{
+  int var;
+  int fil_var = filter(var);
+}
+
+float filter(float val) {  //функция фильтрации
+  Pc = P + varProcess;
+  G = Pc/(Pc + varVolt);
+  P = (1-G)*Pc;
+  Xp = Xe;
+  Zp = Xp;
+  Xe = G*(val-Zp)+Xp; // "фильтрованное" значение
+  return(Xe);
+}
